2b4703230d7c60942e2d2b76dde0af2dde02a284
Haroon Rashid <haroonr@iiitd.ac.in>
2017-06-18 12:30:06 +0530

    formatted

--- wrangle_data.R
+++ wrangle_data.R
@@ -1,32 +1,39 @@
+library(xts)
+library(data.table)
+library(gtools)
+
 create_minutely <- function(){
+  # this function is used to down sample data
   library(gtools)
   month = "default"
-  parent <- "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/IIITD_Jan14_Dec15/"
+  parent <- "/Volumes/MacintoshHD2/Users/"
   dfiles <- mixedsort(list.files(paste0(parent,month,"/"),pattern = "*.csv"))
-  #dfiles <- dfiles[2:length(dfiles)] # removing first first that represents aggregate
-  
   lapply(dfiles, function(x){
     df <- fread(paste0(parent,month,"/",x),header = TRUE)
-    #df_xts <- xts(df$power, as.POSIXct(df$timestamp,tz="Asia/Kolkata",origin = "1970-01-01"))
-    df_xts <- xts(df$power,fasttime::fastPOSIXct(df$timestamp)-19800) # subtracting5:30 hours
-    #remvove duplicates
-    duplicate_rno <- which(duplicated(index(df_xts)))
+    df_xts <- xts(df$power,fasttime::fastPOSIXct(df$timestamp)-19800) # subtracting5:30 according to IST
+    duplicate_rno <- which(duplicated(index(df_xts)))  #remvove duplicates
     if(length(duplicate_rno) > 0 ){
       df_xts <- df_xts[-duplicate_rno,] }
     sampled_df_xts <- resample_data_minutely(df_xts,1) # convert to hourly format
     stopifnot(length(unique(lubridate::second(sampled_df_xts)))==1) # ensures series end at 0 seconds
-    #duplicate_rno <- which(duplicated(index(sampled_df_xts))) # ensure no duplicates are there
-    #sampled_df_xts <- sampled_df_xts[-duplicate_rno,]
-    write.csv(data.frame(timestamp=index(sampled_df_xts),power=round(coredata(sampled_df_xts),3)),file=paste0(parent,"year2016_data","/minutely/",x),row.names = FALSE)
+    write.csv(data.frame(timestamp=index(sampled_df_xts),power=round(coredata(sampled_df_xts),3)),file=paste0(parent,"/minutely/",x),row.names = FALSE)
   })
 }
 
 resample_data_minutely <- function(xts_datap,xminutes) {
-  #downsampled data
-  ds_data <- period.apply(xts_datap,INDEX = endpoints(index(xts_datap)-3600*0.5, on = "minutes", k = xminutes ), FUN= mean) # subtracting half hour to align hours
-  # align data to nearest time boundary
-  # browser()
+  #This function resamples input xts data to xminutes rate
+  ds_data <- period.apply(xts_datap,INDEX = endpoints(index(xts_datap)-3600*0.5, on = "minutes", k = xminutes ), FUN= mean) # subtracting half hour to align IST hours
   align_data <- align.time(ds_data,xminutes*60) # aligning to x seconds
   rm(ds_data)
   return(align_data)
+}
+
+interpolate_missing_readings <- function(org_xts,samp_duration) {
+  # function used to fill missing values by interpolation
+  # input-  org_xts <- xts data, samp_duration: sampling duration of input data
+  timerange = seq(start(org_xts),end(org_xts), by = samp_duration) # assuming original object is hourly sampled
+  temp = xts(rep(NA,length(timerange)),timerange)
+  complete_xts = merge(org_xts,temp)[,1]
+  colnames(complete_xts) <- NULL
+  return(complete_xts)
 }
